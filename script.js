143a3d2ca0beffc053d9a7354838439e6640d874
Francesco Bedussi <francesco@francescobedussi.it>
2016-12-09 15:33:51 +0100

    init after agumenting for sake of clarity

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var menuLayer = Object.create(Layer);
-menuLayer.init({selector: '#menu', opener: '#menuButton', closer: '#menuClose'});
+
 menuLayer.open = function() {
     console.log('agument');
     Layer.open.call(this);
 }
+
+menuLayer.init({selector: '#menu', opener: '#menuButton', closer: '#menuClose'});
 
 var searchLayer = Object.create(Layer);
 searchLayer.init({selector: '#search', opener: '#searchButton', closer: '#searchClose', direction: 'toBottom'});
