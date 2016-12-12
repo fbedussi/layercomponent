562dfc216c6d206fe7ef7fa30644dfdf89257102
Francesco Bedussi <francesco@francescobedussi.it>
2016-12-12 15:02:48 +0100

    fix vari

--- layer.js
+++ layer.js
@@ -1,63 +1,60 @@
-window.Layer = {
+var Layer = {
     isOpen: false,
+    _layerOpen: {},
     
-    layerOpen: {}, //static
-    
-    getLayerOpen: function() {
-        return this.layerOpen;
-    },
-    
-    setLayerOpen: function (layerObj) {
-        proto = this;
-        while (!proto.hasOwnProperty('layerOpen')) {
+    setLayerOpen: function(layerObj) {
+        var proto = this;
+        while (!proto.hasOwnProperty('_layerOpen')) {
             proto = Object.getPrototypeOf(proto);
         }
-        
-        proto.layerOpen = layerObj;
+        proto._layerOpen = layerObj;  
     },
     
-    open: function () {
+    getLayerOpen: function() {
+        return this._layerOpen;
+    },
+    
+    open: function() {
         function executeOpen() {
             switch (this.direction) {
-            case 'toRight':
-                this.layerEl.style = 'transform: translateX(100%)';
-                break;
-            case 'toLeft':
-                this.layerEl.style = 'transform: translateX(-100%)';
-                break;
-            case 'toBottom':
-                this.layerEl.style = 'transform: translateY(100%)';
-                break;
+                case 'toRight':
+                    this.layerEl.style = 'transform: translateX(100%)';
+                    break;
+                case 'toLeft':
+                    this.layerEl.style = 'transform: translateX(-100%)';
+                    break;
+                case 'toBottom':
+                    this.layerEl.style = 'transform: translateY(100%)';
+                    break;
             }
             this.isOpen = true;
             this.setLayerOpen(this);
         }
-        
+
         var wait = 0;
-        
+
         if (this.getLayerOpen().close) {
             this.getLayerOpen().close();
             wait = this.timer;
         }
-
+        
         setTimeout(executeOpen.bind(this), wait);
     },
     
-    close: function () {
-        this.setLayerOpen({});
-        this.isOpen = false;
-        this.layerEl.style = '';
+    close: function() {
+      this.setLayerOpen({});
+      this.isOpen = false;
+      this.layerEl.style = '';
     },
     
-    init: function (options) {
+    init: function(options) {
         this.selector = options.selector;
         this.opener = options.opener;
         this.closer = options.closer;
         this.layerEl = document.querySelector(this.selector);
         this.direction = options.direction || 'toRight';
-        this.timer = parseFloat(window.getComputedStyle(document.querySelector(this.selector))
-            .transitionDuration) * 1000;
-    
+        this.timer = parseFloat( window.getComputedStyle(document.querySelector( this.selector )).transitionDuration) * 1000;
+        
         [].forEach.call(document.querySelectorAll(this.opener), (el) => {
             el.addEventListener('click', () => {
                 if (!this.isOpen) {
@@ -65,14 +62,16 @@ window.Layer = {
                 } else {
                     this.close();
                 }
+              });
             });
-        });
         [].forEach.call(document.querySelectorAll(this.closer), (el) => {
             el.addEventListener('click', (e) => {
                 if (this.isOpen) {
-                    this.close();
+                  this.close();
                 }
             });
         });
+        
+        return this;
     }
-};
+}
